130d91fad8cc9e37359e023f518c6a017af81f1b
Aristia0930 <165614389+Aristia0930@users.noreply.github.com>
2024-12-14 15:41:16 +0900

    기본 리덕스
    기본 리덕스

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Counter from './componets/Counter';
-import Todos from './componets/Todos';
+import Counter from './components/Counter';
+import './App.css';
+import Todos from './components/Todos';
+import CounterContainers from './containers/CounterContainers';
+import TodoContainer from './containers/TodoContainer';
+import Counter2 from './components/Counter2';
 function App() {
-
   return (
     <div className="App">
-        <Counter/>
-        <hr></hr>
-        <Todos/>
+      <CounterContainers></CounterContainers>
+      <hr></hr>
+      <Counter2/>
+
+      {/* <Counter number={0}/> */}
+      <hr></hr>
+      <TodoContainer/>
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import  store  from './slices/store'
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import {Provider} from 'react-redux';
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}> {/* store를 Provider로 감싸기 */}
+    <Provider store={store}>
     <App />
-  </Provider>,
+</  Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
